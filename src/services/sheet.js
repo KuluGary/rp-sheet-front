c2ff064ff8af90e035f513e96e5e11145c7d119a
Gary <kululu.gary@gmail.com>
2023-08-08 21:30:50 +0200

    added env variables

--- src/services/sheet.js
+++ src/services/sheet.js
@@ -1,8 +1,10 @@
+const ENDPOINT_URL = import.meta.env.VITE_SERVER_URL;
+
 export const downloadPdf = async (data) => {
   const headers = new Headers();
   headers.append("Content-Type", "application/json");
 
-  return await fetch("http://localhost:3001/api/v1/dnd5e/pdf", {
+  return await fetch(`´${ENDPOINT_URL}/api/v1/dnd5e/pdf`, {
     headers,
     method: "POST",
     body: JSON.stringify(data),
